4cb6ae5cefc1e571b6cd577604a82ea0e499c7c0
Zulkiflee <s67319010002@ptnicec.ac.th>
2025-03-14 02:11:13 +0700

    ีup

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-
 function changeBgColor() {
     let randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
     document.body.style.backgroundColor = randomColor;
-}
 
+    // เปลี่ยนสีของ btx ให้ตรงกับสีพื้นหลัง
+    document.querySelector(".btx").style.backgroundColor = randomColor;
 
-function showMessage() {
-    alert("สวัสดี ฉันชื่อซุล!");
+    // แสดงรหัสสีที่เปลี่ยน
+    document.getElementById("colorCode").innerText = "สีปัจจุบัน: " + randomColor;
 }
